d901b57b7475e7d858d815d0569907c5c0a08c89
cooderyg <ygk9604@naver.com>
2023-04-02 10:53:50 +0900

    UI 완료

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,24 +1,27 @@
 import type { NextPage } from "next";
-import Layout from "../components/layout";
+import Link from "next/link";
+import Layout from "../../components/layout";
 
 const Chats: NextPage = () => {
-    return (
-        <Layout title="채팅" hasTabBar>
-            <div className="py-10  divide-y-[1px]">
-                {[1, 1, 1, 1, 1, 1, 1, 1].map((_, i)=> (
-                <div key={i}
-                className="px-4 flex cursor-pointer py-3 items-center space-x-3">
-                    <div className="w-12 h-12 rounded-full bg-slate-300" />
-                    <div>
-                        <p className="text-gray-700">Steve Jebs</p>
-                        <p className="text-sm text-gray-500">내일 오후 2시에 모퉁이에서 만나요</p>
-                    </div>
-                </div>
-                ))}
+  return (
+    <Layout hasTabBar title="채팅">
+      <div className="divide-y-[1px] ">
+        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Link href={`/chats/${i}`} key={i}>
+            <div className="flex px-4 cursor-pointer py-3 items-center space-x-3">
+              <div className="w-12 h-12 rounded-full bg-slate-300" />
+              <div>
+                <p className="text-gray-700">Steve Jebs</p>
+                <p className="text-sm  text-gray-500">
+                  See you tomorrow in the corner at 2pm!
+                </p>
+              </div>
             </div>
-        </Layout>
-    )
+          </Link>
+        ))}
+      </div>
+    </Layout>
+  );
+};
 
-}
-
-export default Chats
+export default Chats;
