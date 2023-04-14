f3aae2de03c619ff11f4c1fb316a7b45a345eefb
cooderyg <ygk9604@naver.com>
2023-04-14 19:36:16 +0900

    enter 로그인 회원 정보 db 생성

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,29 +1,65 @@
 import client from "@/libs/server/client";
 import withHandler from "@/libs/server/withHandler";
+import { create } from "domain";
 import { NextApiRequest, NextApiResponse } from "next";
+
 
 async function handler (
     req:NextApiRequest, res:NextApiResponse){
-   const { phone, email } = JSON.parse(req.body);
-   let user;
-   if(email) {
-     user = await client.user.findUnique({
-        where: {
-            email,
+   const { phone, email } = req.body;
+
+   const payload = phone ? {phone: +phone} : {email}
+   const user = await client.user.upsert({
+        where:{
+            ...payload
+
         },
-    });
-    if(!user){
-        console.log("찾지 못했습니다. 만들겠습니다.");
-        await client.user.create({
-            data: {
-                name:"Anonymous",
-                email,
-            },
-        });
+        create: {
+            name:"홍길동",
+            ...payload
+        },
+        update:{},
+    })      
+    console.log(user)
+//    if(email) {
+//      user = await client.user.findUnique({
+//         where: {
+//             email,
+//         },
+//     });
+//     if(user) console.log("found it")
+//     if(!user){
+//         console.log("찾지 못했습니다. 만들겠습니다.");
+//         await client.user.create({
+//             data: {
+//                 name:"Anonymous",
+//                 email,
+//             },
+//         });
        
-    }
-    console.log(user);
-   }
+//     }
+//     console.log(user);
+//    }
+//    if(phone) {
+//     user = await client.user.findUnique({
+//        where: {
+//             phone:Number(phone),
+//        },
+//    });
+//    if(user) console.log("found it")
+//    if(!user){
+//        console.log("찾지 못했습니다. 만들겠습니다.");
+//        await client.user.create({
+//            data: {
+//                name:"Anonymous",
+//                phone:Number(phone),
+//            },
+//        });
+    
+//    }
+//     console.log(user);
+//   }
+
    return res.status(200).end();
 }
 
