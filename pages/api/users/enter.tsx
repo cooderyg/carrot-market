7b9d84d78a022035014501b03ef0c17d5363f927
cooderyg <ygk9604@naver.com>
2023-04-19 11:55:48 +0900

    token 생성 logic & client 리팩터링

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,66 +1,39 @@
 import client from "@/libs/server/client";
-import withHandler from "@/libs/server/withHandler";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { randomInt } from "crypto";
 import { create } from "domain";
 import { NextApiRequest, NextApiResponse } from "next";
 
 
+
 async function handler (
-    req:NextApiRequest, res:NextApiResponse){
+    req:NextApiRequest,
+    res:NextApiResponse<ResponseType>
+    ){
    const { phone, email } = req.body;
-
-   const payload = phone ? {phone: +phone} : {email}
-   const user = await client.user.upsert({
-        where:{
-            ...payload
-
+   const user = phone ? {phone: +phone} :email? {email} : null;
+   if(!user) return res.status(400).json({ok: false});
+   const payload = Math.floor(10000 + Math.random() * 900000) + "";
+    const token = await client.token.create({
+        data: {
+            payload,
+            user: {
+                connectOrCreate: {    
+                    where:{
+                        ...user
+            
+                    },
+                    create: {
+                        name:"홍길동",
+                        ...user
+                    },},
+            },
         },
-        create: {
-            name:"홍길동",
-            ...payload
-        },
-        update:{},
-    })      
-    console.log(user)
-//    if(email) {
-//      user = await client.user.findUnique({
-//         where: {
-//             email,
-//         },
-//     });
-//     if(user) console.log("found it")
-//     if(!user){
-//         console.log("찾지 못했습니다. 만들겠습니다.");
-//         await client.user.create({
-//             data: {
-//                 name:"Anonymous",
-//                 email,
-//             },
-//         });
-       
-//     }
-//     console.log(user);
-//    }
-//    if(phone) {
-//     user = await client.user.findUnique({
-//        where: {
-//             phone:Number(phone),
-//        },
-//    });
-//    if(user) console.log("found it")
-//    if(!user){
-//        console.log("찾지 못했습니다. 만들겠습니다.");
-//        await client.user.create({
-//            data: {
-//                name:"Anonymous",
-//                phone:Number(phone),
-//            },
-//        });
-    
-//    }
-//     console.log(user);
-//   }
+    });
 
-   return res.status(200).end();
+   return res.json({
+    ok: true,
+   });
 }
 
 export default withHandler("POST", handler);
