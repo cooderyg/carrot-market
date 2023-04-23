14be48e7036aad664b36c5aa332a420a08976c34
cooderyg <ygk9604@naver.com>
2023-04-23 11:39:53 +0900

    community end

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -7,6 +7,7 @@ import useMutation from "@/libs/client/useMutation";
 import { useEffect } from "react";
 import { Post } from "@prisma/client";
 import { useRouter } from "next/router";
+import useCoords from "@/libs/client/useCoords";
 
 
 
@@ -21,18 +22,19 @@ interface WriteResponse {
 }
 
 const Write: NextPage = () => {
+  const {latitude, longitude} = useCoords();
   const { register, handleSubmit } = useForm<WriteForm>();
   const [post, { loading, data }] =useMutation<WriteResponse>("/api/posts");
   const  router = useRouter();
   const onValid = (data: WriteForm) => {
     if(loading) return;
-    post(data)
+    post({...data, latitude, longitude})
   }
   useEffect(()=> {
     if(data && data.ok) {
       router.push(`/community/${data.post.id}`)
     }
-  }, [data ,router])
+  }, [data, router])
   return (
     <Layout canGoBack title="Write Post">
       <form onSubmit={handleSubmit(onValid)} className="p-4 space-y-4">
