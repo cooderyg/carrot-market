1374722e29a3ed3181abd72e7a02a953afe8f5b8
cooderyg <ygk9604@naver.com>
2023-04-27 13:59:36 +0900

    이미지, 스트리밍영상 제외 완료

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -3,22 +3,75 @@ import Layout from "../../components/layout";
 import Message from "../../components/message";
 import useSWR from "swr";
 import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import useMutation from "@/libs/client/useMutation";
 import { Stream } from "@prisma/client";
+import useUser from "@/libs/client/useUser";
+import { useEffect } from "react";
+
+interface StreamMessage {
+    message: string;
+    id: number;
+    user: {
+      avartar?: string;
+      id: number;
+    };
+}
+
+interface StreamWithMessages extends Stream{
+  messages: StreamMessage[]
+}
 
 interface StreamResponse {
   ok: true;
-  stream : Stream
+  stream : StreamWithMessages;
 }
 
-const Stream: NextPage = () => {
-  
+interface MessageForm{
+  message: string; 
+
+}
+
+
+
+const Streams: NextPage = () => {
+  const {user} = useUser();
   const router = useRouter();
-  const { data } = useSWR<StreamResponse>(
+  const { register, handleSubmit,  reset } = useForm<MessageForm>();
+  const { data, mutate } = useSWR<StreamResponse>(
     router.query.id 
     ? `/api/streams/${router.query.id}` 
-    : null
-    )
-  console.log(router.query.id)
+    : null,
+    {
+      refreshInterval: 1000,
+    }
+    );
+  const [sendMessage, { loading, data: sendMessageData }] = useMutation(`/api/streams/${router.query.id}/messages`);
+  const onvalid = (form:MessageForm)=> {
+    if(loading) return;
+    reset();
+    mutate(prev => prev && ({
+        ...prev, 
+        stream: {
+          ...prev.stream,
+          messages:[
+            ...prev.stream.messages,
+            {
+              id: Date.now(),
+              message: form.message,
+              user: {
+                ...user
+              },
+            },
+          ],
+        },
+      } as any
+      ), false);
+    sendMessage(form);
+  }
+
+
+
   return (
     <Layout canGoBack>
       <div className="py-10 px-4  space-y-4">
@@ -33,14 +86,15 @@ const Stream: NextPage = () => {
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Live Chat</h2>
           <div className="py-10 pb-16 h-[50vh] overflow-y-scroll  px-4 space-y-4">
-            <Message message="Hi how much are you selling them for?" />
-            <Message message="I want ￦20,000" reversed />
-            <Message message="미쳤어" />
+            {data?.stream.messages.map(message =>
+            <Message key={message?.id}  message={message?.message}  reversed={message.user.id === user?.id}/>
+            )}
           </div>
           <div className="fixed py-2 bg-white  bottom-0 inset-x-0">
-            <div className="flex relative max-w-md items-center  w-full mx-auto">
+            <form onSubmit={handleSubmit(onvalid)} className="flex relative max-w-md items-center  w-full mx-auto">
               <input
                 type="text"
+                {...register("message", { required:true, })}
                 className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
               />
               <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
@@ -48,7 +102,7 @@ const Stream: NextPage = () => {
                   &rarr;
                 </button>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
@@ -56,4 +110,4 @@ const Stream: NextPage = () => {
   );
 };
 
-export default Stream;
+export default Streams;
